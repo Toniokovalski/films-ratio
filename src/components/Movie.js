049af50af3f00f804ce23e6314f582fc5cda28c2
Anton Koval <toniokovalski@gmail.com>
2023-08-09 14:19:07 +0200

    handle case when film has no image

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,12 @@
+import noImage from "./../img/no-film-img.jpg";
+
 export function Movie({ movie, onSelectMovie }) {
   return (
     <li onClick={() => onSelectMovie(movie.imdbID)}>
-      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <img
+        src={movie.Poster !== "N/A" ? movie.Poster : noImage}
+        alt={`${movie.Title} poster`}
+      />
       <h3>{movie.Title}</h3>
       <div>
         <p>
